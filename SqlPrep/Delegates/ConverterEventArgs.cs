8537fcaa657ea33f06971decdc5391c5e7f973b1
Will Kraft <pwizard@gmail.com>
2019-12-01 14:24:42 -0600

    Added headers to code

--- SqlPrep/Delegates/ConverterEventArgs.cs
+++ SqlPrep/Delegates/ConverterEventArgs.cs
@@ -1,15 +1,30 @@
-﻿
+﻿/*
+   This file is part of SqlPrep.
+   Copyright © 2019, Will Kraft <pwizard@gmail.com>
+
+    SqlPrep is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SqlPrep is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SqlPrep.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using System;
-using System.Drawing;
-using System.Windows.Media;
 using Brush = System.Windows.Media.Brush;
 
 namespace SqlPrep.Delegates
 {
-	/// <summary>
-	/// Description of ConvertEventArgs.
-	/// </summary>
-	public class ConverterEventArgs : EventArgs
+    /// <summary>
+    /// Description of ConvertEventArgs.
+    /// </summary>
+    public class ConverterEventArgs : EventArgs
 	{
 		public ConverterEventArgs()
 		{
